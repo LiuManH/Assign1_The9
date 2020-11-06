4e792f886a359ca3052f88ccd8baa8739d226203
LiuManH <2639089532@qq.com>
2020-11-06 10:46:04 +0800

    The9

--- app/Models/People.php
+++ app/Models/People.php
@@ -13,6 +13,9 @@ class People {
         '1' => [
             'id' => 1,
             'name' => 'YuXin Liu',
+            'height' =>'168',
+            'age' =>'23',
+            'birthplace' =>'GuiZhou',
             'description' => 'Liu Yuxin was born on April 20, 1997 in Guizhou Province. She is a female singer in mainland China',
             'link' => 'https://baike.so.com/doc/5338868-5574309.html',
             'image' => 'liu.jpg',
@@ -20,6 +23,9 @@ class People {
         '2' => [
             'id' => 2,
             'name' => 'YuShu Xin',
+            'height' =>'168',
+            'age' =>'25',
+            'birthplace' =>'ShangHai',
             'description' => 'Yu Shuxin (born December 18, 1995 in Shanghai) is a Chinese actress',
             'link' => 'https://baike.so.com/doc/24208390-24795851.html',
             'image' => 'yushuxin.jpg',
@@ -27,6 +33,9 @@ class People {
         '3' => [
             'id' => 3,
             'name' => 'JiaQi Xu',
+            'height' =>'170',
+            'age' =>'25',
+            'birthplace' =>'ZheJiang',
             'description' => 'Xu Jiaqi was born in Taizhou, Zhejiang province on August 27, 1995. She graduated from Shanghai Normal University. She is a female singer in mainland China',
             'link' => 'https://baike.so.com/doc/7050327-7273233.html',
             'image' => 'xu.jpg',
@@ -34,6 +43,9 @@ class People {
         '4' => [
             'id' => 4,
             'name' => 'Yan Yu',
+            'height' =>'172',
+            'age' =>'23',
+            'birthplace' =>'BeiJin',
             'description' => 'Yu Yan was born in Beijing on May 26, 1997. She is a Chinese singer and actress',
             'link' => 'https://baike.so.com/doc/445480-471693.html',
             'image' => 'yuyan.jpg',
@@ -41,6 +53,9 @@ class People {
         '5' => [
             'id' => 5,
             'name' => 'KeYin Xie',
+            'height' =>'168',
+            'age' =>'23',
+            'birthplace' =>'SiChuan',
             'description' => 'Xie Keyin, born on January 4, 1997 in Sichuan Province, is a Chinese mainland female singer',
             'link' => 'https://baike.so.com/doc/29778164-31329380.html',
             'image' => 'xie.jpg',
@@ -48,6 +63,9 @@ class People {
              '6' => [
             'id' => 4,
             'name' => 'Qi An',
+            'height' =>'158',
+            'age' =>'24',
+            'birthplace' =>'ChongQing',
             'description' => 'AnQi was born in Chongqing on May 13,1996. She is a Chinese mainland pop singer',
             'link' => 'https://baike.so.com/doc/28454680-29889754.html',
             'image' => 'anqi.jpg',
