a903e783db34905d61075436aa0f65f6fc024c38
LiuManH <2639089532@qq.com>
2020-11-06 10:49:21 +0800

    The9

--- app/Views/onepeople.php
+++ app/Views/onepeople.php
@@ -7,6 +7,9 @@
 		<h1>One people</h1>
 		<p><strong>ID:</strong> {id}</p>
 		<p><strong>Name:</strong> {name}</p>
+                <p><strong>Height:</strong> {height}</p>
+                <p><strong>Age:</strong> {age}</p>
+                <p><strong>Birthplace:</strong> {birthplace}</p>
 		<p><strong>Description:</strong> {description}</p>
 		<p><strong>Tourism site:</strong> {link}</p>
 		<p><img src="/image/{image}"/></p>
